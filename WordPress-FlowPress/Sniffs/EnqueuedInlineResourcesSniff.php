0da341090e9d9ff3c52ce03719bc1c6d9d812664
Bart Dabek <bart@flowpress.com>
2017-01-26 09:58:06 -0500

    New Sniff Test

---
+++ WordPress-FlowPress/Sniffs/EnqueuedInlineResourcesSniff.php
@@ -0,0 +1,28 @@
+<?php
+class WordPress_Sniffs_WP_EnqueuedResourcesSniff implements PHP_CodeSniffer_Sniff {
+
+	public function register() {
+		return array(
+			T_CONSTANT_ENCAPSED_STRING,
+			T_DOUBLE_QUOTED_STRING,
+			T_INLINE_HTML,
+		);
+
+	}
+
+	public function process( PHP_CodeSniffer_File $phpcsFile, $stackPtr ) {
+		$tokens = $phpcsFile->getTokens();
+		$token  = $tokens[ $stackPtr ];
+
+		// if ( preg_match( '#rel=[\'"]?stylesheet[\'"]?#', $token['content'] ) > 0 ) {
+		// 	$phpcsFile->addError( 'Stylesheets must be registered/enqueued via wp_enqueue_style', $stackPtr, 'NonEnqueuedStylesheet' );
+		// }
+
+		if ( preg_match( '#<script[^>]*(?<=src=)#', $token['content'] ) > 0 ) {
+			$phpcsFile->addError( 'Scripts must be registered/enqueued via wp_enqueue_script', $stackPtr, 'NonEnqueuedScript' );
+		}
+		var_dump('test');
+
+	} // End process().
+
+} // End class.
