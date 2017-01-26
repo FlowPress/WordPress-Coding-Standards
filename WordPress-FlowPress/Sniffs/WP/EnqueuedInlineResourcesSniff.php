33596991f9ab339704561e47508a119c9f7e5ef8
Bart Dabek <bart@flowpress.com>
2017-01-26 13:17:58 -0500

    test

--- WordPress-FlowPress/Sniffs/WP/EnqueuedInlineResourcesSniff.php
+++ WordPress-FlowPress/Sniffs/WP/EnqueuedInlineResourcesSniff.php
@@ -15,11 +15,11 @@ class FlowPress_Sniffs_WP_EnqueuedInlineResourcesSniff implements PHP_CodeSniffe
 		$token  = $tokens[ $stackPtr ];
 // var_dump($token);
 		// if ( preg_match( '#rel=[\'"]?stylesheet[\'"]?#', $token['content'] ) > 0 ) {
-			$phpcsFile->addError( 'Stylesheets must be registered/enqueued via wp_enqueue_style', $stackPtr, 'NonEnqueuedStylesheet' );
+			$phpcsFile->addError( 'Stylesheets must be registered/enqueued via wp_enqueue_style ' . $stackPtr . $token, $stackPtr, 'NonEnqueuedStylesheet' );
 		// }
 
-		$phpcsFile->addError( 'Stylesheets must be registered/enqueued via wp_enqueue_style $stackPtr ' . $stackPtr, $stackPtr, 'NonEnqueuedStylesheet' );
-		$phpcsFile->addError( 'Stylesheets must be registered/enqueued via wp_enqueue_style  $token ' . $token, $stackPtr, 'NonEnqueuedStylesheet' );
+		// $phpcsFile->addError( 'Stylesheets must be registered/enqueued via wp_enqueue_style $stackPtr ' . $stackPtr, $stackPtr, 'NonEnqueuedStylesheet' );
+		// $phpcsFile->addError( 'Stylesheets must be registered/enqueued via wp_enqueue_style  $token ' . $token, $stackPtr, 'NonEnqueuedStylesheet' );
 
 		// if ( preg_match( '#<script[^>]*(?<=src=)#', $token['content'] ) > 0 ) {
 			// $phpcsFile->addError( 'Scripts must be registered/enqueued via wp_enqueue_script', $stackPtr, 'NonEnqueuedScript' );
