6e02dff15bfba290d95e8049275e3d6862b59fd4
Bart Dabek <bart@flowpress.com>
2017-01-26 13:17:04 -0500

    debug

--- WordPress-FlowPress/Sniffs/WP/EnqueuedInlineResourcesSniff.php
+++ WordPress-FlowPress/Sniffs/WP/EnqueuedInlineResourcesSniff.php
@@ -13,10 +13,13 @@ class FlowPress_Sniffs_WP_EnqueuedInlineResourcesSniff implements PHP_CodeSniffe
 	public function process( PHP_CodeSniffer_File $phpcsFile, $stackPtr ) {
 		$tokens = $phpcsFile->getTokens();
 		$token  = $tokens[ $stackPtr ];
-var_dump($token);
+// var_dump($token);
 		// if ( preg_match( '#rel=[\'"]?stylesheet[\'"]?#', $token['content'] ) > 0 ) {
 			$phpcsFile->addError( 'Stylesheets must be registered/enqueued via wp_enqueue_style', $stackPtr, 'NonEnqueuedStylesheet' );
 		// }
+
+		$phpcsFile->addError( 'Stylesheets must be registered/enqueued via wp_enqueue_style $stackPtr ' . $stackPtr, $stackPtr, 'NonEnqueuedStylesheet' );
+		$phpcsFile->addError( 'Stylesheets must be registered/enqueued via wp_enqueue_style  $token ' . $token, $stackPtr, 'NonEnqueuedStylesheet' );
 
 		// if ( preg_match( '#<script[^>]*(?<=src=)#', $token['content'] ) > 0 ) {
 			// $phpcsFile->addError( 'Scripts must be registered/enqueued via wp_enqueue_script', $stackPtr, 'NonEnqueuedScript' );
