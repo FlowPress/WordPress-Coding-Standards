44d5be2dc1c339272ba157673c6928cd6633baec
Bart Dabek <bart@flowpress.com>
2017-09-08 15:27:59 -0400

    Our rules

--- FlowPress/Sniffs/WP/EnqueuedInlineResourcesSniff.php
+++ FlowPress/Sniffs/WP/EnqueuedInlineResourcesSniff.php
@@ -14,12 +14,10 @@ class FlowPress_Sniffs_WP_EnqueuedInlineResourcesSniff implements PHP_CodeSniffe
 		$tokens = $phpcsFile->getTokens();
 		$token  = $tokens[ $stackPtr ];
 
-		var_dump($token);
-		var_dump($stackPtr);
-
+		if ( strpos($token['content'], '<style') !== false ) {
 		// if ( preg_match( '#rel=[\'"]?stylesheet[\'"]?#', $token['content'] ) > 0 ) {
-			$phpcsFile->addError( 'Stylesheets must be registered/enqueued via wp_enqueue_style', $stackPtr, 'NonEnqueuedStylesheet' );
-		// }
+			$phpcsFile->addError( '123Stylesheets must be registered/enqueued via wp_enqueue_style', $stackPtr, 'NonEnqueuedStylesheet' );
+		}
 
 		// if ( preg_match( '#<script[^>]*(?<=src=)#', $token['content'] ) > 0 ) {
 			// $phpcsFile->addError( 'Scripts must be registered/enqueued via wp_enqueue_script', $stackPtr, 'NonEnqueuedScript' );
